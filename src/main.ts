30f8916411c6fa00dc11d128f98e602e36c99227
Dominic Arrojado <dominicarrojado@gmail.com>
2022-07-07 16:24:51 +0800

    implement validation when creating and updating a link

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,6 +6,8 @@ async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT;
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port);
 
